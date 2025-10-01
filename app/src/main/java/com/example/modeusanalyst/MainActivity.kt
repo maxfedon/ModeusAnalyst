25d5d9bc2aea98622fab0086e184c080c50b861b
Max <fedonkin145@gmail.com>
2025-10-01 16:54:28 +0500

    Init project

---
+++ app/src/main/java/com/example/modeusanalyst/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.modeusanalyst
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.modeusanalyst.ui.theme.ModeusAnalystTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ModeusAnalystTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(R.drawable.layout),
+        contentDescription = ""
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ModeusAnalystTheme {
+        Greeting("Android")
+    }
+}
